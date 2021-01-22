3915b2283c7f38f635a0fc0eaa87042e3334e49d
JoshLi <542938192@qq.com>
2021-01-22 11:47:07 +0800

    添加Android部分

---
+++ UniPlugin-JCore-Android/uniplugin_jcore/src/main/java/cn/jiguang/uniplugin_jcore/JCoreModule.java
@@ -0,0 +1,10 @@
+package cn.jiguang.uniplugin_jcore;
+
+import io.dcloud.feature.uniapp.common.UniDestroyableModule;
+
+public class JCoreModule extends UniDestroyableModule{
+    @Override
+    public void destroy() {
+
+    }
+}
