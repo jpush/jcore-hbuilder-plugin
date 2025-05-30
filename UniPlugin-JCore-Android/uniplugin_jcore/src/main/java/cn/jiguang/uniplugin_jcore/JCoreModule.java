347bd838dfe4ebc17104c59a4a479dc463972eb6
huangshuni <15889638723@163.com>
2025-05-30 19:19:30 +0800

    升级安卓jcore502, 插件版本修改为1.2.3

--- UniPlugin-JCore-Android/uniplugin_jcore/src/main/java/cn/jiguang/uniplugin_jcore/JCoreModule.java
+++ UniPlugin-JCore-Android/uniplugin_jcore/src/main/java/cn/jiguang/uniplugin_jcore/JCoreModule.java
@@ -6,6 +6,8 @@ import cn.jiguang.api.JCoreInterface;
 import cn.jiguang.api.utils.JCollectionAuth;
 import io.dcloud.feature.uniapp.annotation.UniJSMethod;
 import io.dcloud.feature.uniapp.common.UniDestroyableModule;
+import com.alibaba.fastjson.JSONObject;
+import com.taobao.weex.bridge.JSCallback;
 
 public class JCoreModule extends UniDestroyableModule{
     public static Context uniContext = null;
@@ -27,6 +29,28 @@ public class JCoreModule extends UniDestroyableModule{
         updatePluginStatu();
         JCoreInterface.testCountryCode(uniContext,code);
     }
+
+    @UniJSMethod(uiThread = true)
+    public void enableSDKLocalLog(JSONObject readableMap) {
+        updatePluginStatu();
+        if (readableMap == null) {
+            return;
+        }
+        boolean enable = readableMap.getBoolean("enable");
+        boolean uploadJgToServer = readableMap.getBoolean("uploadJgToServer");
+        JCoreInterface.enableSDKLocalLog(mWXSDKInstance.getContext(), enable, uploadJgToServer);
+    }
+
+    @UniJSMethod(uiThread = true)
+    public void readNewLogs(JSCallback callback) {
+        updatePluginStatu();
+        if (callback == null) {
+            return;
+        }
+        String logs = JCoreInterface.readNewLogs(mWXSDKInstance.getContext());
+        callback.invoke(logs);
+    }
+
     @Override
     public void destroy() {
 
